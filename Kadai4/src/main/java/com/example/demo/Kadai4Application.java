af87a6fa4d8361301c007b26c4428dfe0955f718
oyama <oyama@192.168.10.103>
2025-02-14 17:58:34 +0900

    step7提出

---
+++ Kadai4/src/main/java/com/example/demo/Kadai4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kadai4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kadai4Application.class, args);
+	}
+
+}
